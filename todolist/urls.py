e3005cf73371aa567f5540f39111d01126823fae
Eugene Polunin <altahoma@gmail.com>
2023-07-26 12:46:00 +0300

    Implemented task creation

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TaskList, TagList
+from .views import TaskList, TaskCreateView, TagList
 
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagList.as_view(), name="tag-list"),
 ]
 
--- todolist/views.py
+++ todolist/views.py
@@ -7,5 +7,10 @@ class TaskList(generic.ListView):
     model = Task
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+
+
 class TagList(generic.ListView):
     model = Tag

7e0708cdf4c14b829fb74075752f6d57683aa36c
Eugene Polunin <altahoma@gmail.com>
2023-07-26 13:44:33 +0300

    Implemented task toggle as done or not done

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import TaskList, TaskCreateView, TagList
-
+from .views import TaskList, TaskCreateView, TagList, toggle_task_completion
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/toggle/", toggle_task_completion, name="toggle-task"),
     path("tags/", TagList.as_view(), name="tag-list"),
 ]
 
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 
 from .models import Task, Tag
+from .forms import TaskForm
 
 
 class TaskList(generic.ListView):
@@ -9,8 +13,17 @@ class TaskList(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
+    template_name = "todolist/task_form.html"
+    success_url = reverse_lazy('todolist:task-list')
 
 
 class TagList(generic.ListView):
     model = Tag
+
+
+def toggle_task_completion(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todolist:task-list"))

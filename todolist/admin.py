7b5a10cee78c4e7bed44a717e630fcd176b02ee2
Eugene Polunin <altahoma@gmail.com>
2023-07-26 11:21:53 +0300

    Registered todolist app in settings.py and admin.py

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "created",
+        "deadline",
+        "is_done",
+    )
+    list_filter = ("is_done", "tags")
+    search_fields = ("content",)
